e958dab9506be0a21ad4717e28fa1012dfa5dbe9
Mayank Gupta <mayankg10010@gmail.com>
2025-11-08 23:58:58 +0530

    Time: 0 ms (100%), Space: 15.7 MB (67.11%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -4,7 +4,8 @@ public:
     	int n = nums.size(), k, l;
     	for (k = n - 2; k >= 0; k--) {
             if (nums[k] < nums[k + 1]) {
-                break;
+                break; 
+                
             }
         }
     	if (k < 0) {
